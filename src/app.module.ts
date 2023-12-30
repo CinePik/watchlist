76cd4dfa046dd9bf48c26a1854b9b76019058e2d
mevljas <sebastjan.mevlja@telekom.si>
2023-12-30 22:08:43 +0100

    add prisma and keycloak

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { KeycloakModule } from './keycloak/keycloak.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: false,
+      isGlobal: true,
+    }),
+    KeycloakModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
